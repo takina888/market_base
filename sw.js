ce3dfc7b35dd7d3feb240e3d81a312ac1fe41ed8
takina888 <takinaito@gmail.com>
2026-07-20 16:27:48 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil((async()=>{for(const k of await caches.keys())await caches.delete(k);await self.clients.claim()})()));self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)))})
